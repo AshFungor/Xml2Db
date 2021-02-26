f334c8a3e6e719a51fd351add2b42de99a35f568
AshFungor <64192682+AshFungor@users.noreply.github.com>
2021-02-26 15:10:07 +0300

    Add files via upload

---
+++ Xml2Db.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xml2Db.Desktop.ViewModels;
+
+namespace Xml2Db.Desktop.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindowViewModel ViewModel => DataContext as MainWindowViewModel;
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+                    ViewModel.Load(dlg.FileName);
+                    MessageBox.Show("Обмен данными завершён успешно!", "Обмен данными завершён", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка обмена данными", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
